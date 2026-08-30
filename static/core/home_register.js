16fd1823c30e8fbb9ad37e430e08f6b8d4ffe540
SantoshP2608 <sarathsantoshpabolu@gmail.com>
2026-08-30 12:27:47 +0530

    Register Animation added

---
+++ static/core/home_register.js
@@ -0,0 +1,15 @@
+document.querySelectorAll(".register-btn").forEach(button => {
+    button.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        const link = button.closest(".register-link");
+
+        button.classList.add("clicked");
+
+        setTimeout(() => {
+            window.location.href = link.href;
+        }, 400);
+    });
+});
+
+console.log("HOME REGISTER JS LOADED");
